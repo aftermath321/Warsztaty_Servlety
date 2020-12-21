d2677e34a40e22ba92164a4065e95fd24d358f91
aftermath321 <77aftermath77@gmail.com>
2020-12-21 15:13:15 +0100

    Servlets

---
+++ src/main/java/pl/coderslab/servlets/UserEdit.java
@@ -0,0 +1,20 @@
+package pl.coderslab.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserEdit", urlPatterns = "/user/edit")
+public class UserEdit extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
